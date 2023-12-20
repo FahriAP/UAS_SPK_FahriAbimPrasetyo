783240bf99b703f235364e3f8ad3ea4c7353f984
FahriAP <129932450+FahriAP@users.noreply.github.com>
2023-12-20 17:45:47 +0700

    Add files via upload

---
+++ UAS/README.md
@@ -0,0 +1,11 @@
+# Tugas UAS SPK
+Nama : Fahri Abim Prasetyo <br>
+NIM : 201011402168 <br>
+Kelas : 07-TPLP-012 <br>
+
+## Install requirements
+```pip install -r requirements.txt```
+
+## Run the app
+Untuk menjalankan:
+```python main.py```
---
+++ UAS/fahri_SPK_Model/fahri.SQl.sql
@@ -0,0 +1,11 @@
+INSERT INTO smartphoneasus (brand,ram,prosesor,storage,baterai,harga,ukuran_layar) VALUES
+	 ('Asus Zenfone 2','2 GB','MediaTek Helio G35','32 GB','5.000 mAh','Rp.1.499.000','6.5 inch'),
+	 ('Asus Zenfone 5Q','4 GB','MediaTek Helio G35','64 GB','6.000 mAh','Rp.1.999.000','6.5 inch'),
+	 ('Asus ROG Phone 7','8 GB','Qualcomm Snapdragon 720G','128 GB','4.500 mAh','Rp.3.999.000','6.4 inch'),
+	 ('Asus ROG Phone 6','8 GB','MediaTek Dimensity 1000+','128 GB','4.500 mAh','Rp.4.999.000','6.55 inch'),
+	 ('Asus Zenfone 4','8 GB','MediaTek Dimensity 1200','128 GB','4.500 mAh','Rp.4.999.000','6.43 inch'),
+	 ('Asus Zenfone 10','12 GB','Qualcomm Snapdragon 865','128 GB','4.200 mAh','Rp.7.999.000','6.44 inch'),
+	 ('Asus Zenfone 3 Max','4 GB','MediaTek Helio G85','64 GB','6.000 mAh','Rp.2.499.000','6.5 inch'),
+	 ('Asus ROG Phone 5','8 GB','MediaTek Dimensity 810','128 GB','5.000 mAh','Rp.3.999.000','6.5 inch'),
+	 ('Asus Zenfone 4 Max','12 GB','Qualcomm Snapdragon 870','128 GB','4.500 mAh','Rp.5.999.000','6.55 inch'),
+	 ('Asus Zenfone 9','12 GB','Qualcomm Snapdragon 898','128 GB','5.000 mAh','Rp.5.970.000','6.62 inch');
